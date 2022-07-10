9ac41f71e1932a28de06603eb363715907114ea3
Mohammad <rezaparvizi1994@gmail.com>
2022-07-10 04:51:13 +0430

    Append entry point main

---
+++ cmd/api/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+const version = "0.1.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "dev", "Environement (prod|staging|dev)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "api", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s listen to the server on %d", cfg.env, cfg.port)
+	err := server.ListenAndServe()
+	logger.Fatal(err)
+}
