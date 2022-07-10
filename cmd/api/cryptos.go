21b02205092f06431a4c42cdd329e5e3afc560a1
Mohammad <rezaparvizi1994@gmail.com>
2022-07-10 17:22:28 +0430

    Impl crypto REST api

---
+++ cmd/api/cryptos.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/jigsaw373/cav/internal/validator"
+)
+
+type validationResult struct {
+	Crypto  string `json:"crypto"`
+	Address string `json:"address"`
+	Valid   bool   `json:"valid"`
+}
+
+func (app *application) validateAddressHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := app.readCryptoParam(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	addr, err := app.readAddressParam(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	valid, err := validator.ValidateAddress(c, addr)
+	if err != nil {
+		app.logger.Println(err)
+		app.failedValidationResponse(w, r, err)
+		return
+	}
+
+	res := validationResult{
+		Crypto:  c,
+		Address: addr,
+		Valid:   valid,
+	}
+	err = app.writeJSON(w, enveloping{"result": res}, http.StatusOK, nil)
+
+	if err != nil {
+		app.logger.Println(err)
+		app.serverErrorResponse(w, r, err)
+	}
+}
